234ed00d8c7a0cbbb4643eee5beefea8ea0197b4
hackr9483 <hackr9483@d6ec5613-2f45-0410-bde3-6b5b92f72b8a>
2008-02-03 15:48:30 +0000

    ReadFile function working.

--- trunk/make_image.asm
+++ trunk/make_image.asm
@@ -10,7 +10,37 @@ times 510-($-sector1)  db 0
 boot_signature: dw 0xaa55 ;boot signature
 
 sector2:
-db "hello there",0
+;directory stuff...
+times 512-($-sector2)  db 0
+
+sector3:
+;first file
+.stype: db 0x0F
+.sectors_used: dd 1
+.filesize: dd 512
+.attrib: dw 0
+.name_size: db 6
+.directory: dd 0
+.fname: db "hello",0
+.fdata:
+db "Hello there Mr. World!",0
+
+times 512-($-sector3) db 0
+
+sector4:
+;second file
+.stype: db 0x0F
+.sectors_used: dd 1
+.filesize: dd 512
+.attrib: dw 0
+.name_size: db 9
+.directory: dd 0
+.fname: db "hi_there",0
+.fdata:
+db "The second file, w00t!",0
+db "The second file, w00t!",0
+db "The second file, w00t!",0
+times 512-($-sector4) db 0
 
 
 end_image:
